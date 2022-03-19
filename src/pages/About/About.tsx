3025f46979410b4ee74282acc8aa716ef9fa600d
Do1phin <do1phin.name@gmail.com>
2022-03-19 20:17:30 +0200

    Added About component

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import './About.styles.scss';
+
+export const About: FC = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <section className='about container'>
+        <div className='about__wrapper'>
+          <div className='about__supervision supervision'>
+            <h2 className='supervision__title'>{t('about.supervision.title')}</h2>
+            <p className='supervision__text'>{t('about.supervision.text')}</p>
+          </div>
+
+          <div className='about__awareness awareness'>
+            <h2 className='awareness__title'>{t('about.awareness.title')}</h2>
+            <p className='awareness__text'>{t('about.awareness.text')}</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
