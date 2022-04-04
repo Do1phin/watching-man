a2fb96352d736df624e6e66bd19d71f35ed0910b
Do1phin <do1phin.name@gmail.com>
2022-04-04 17:01:22 +0300

    Added Input component

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,57 @@
+import React, { ReactNode } from 'react';
+import cx from 'classnames';
+
+import './Input.scss';
+
+export enum Type {
+  TEXT = 'text',
+  PASSWORD = 'password',
+  NUMBER = 'number',
+}
+
+export enum IconPosition {
+  START = 'start',
+  END = 'end',
+}
+
+export enum Status {
+  NONE = 'none',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+
+interface IProps {
+  value?: string;
+  type?: Type;
+  disabled?: boolean;
+  placeholder?: string;
+  status?: Status;
+  icon?: ReactNode;
+  iconPosition?: IconPosition;
+  onChange: Function;
+  onClick?: Function;
+}
+
+export const Input = (props: IProps) => {
+  const { value, type, disabled, icon, status, placeholder, onChange, onClick, iconPosition } =
+    props;
+
+  return (
+    <>
+      <div className='input__wrapper'>
+        {icon && <div className={cx('input__icon', iconPosition)}>{icon}</div>}
+        <input
+          type={type}
+          className='input'
+          status={status}
+          autoComplete='off'
+          disabled={disabled}
+          placeholder={placeholder}
+          onClick={onClick}
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    </>
+  );
+};
