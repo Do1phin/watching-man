d7c2d9c6bafe9acd8c5cccce6fb6516b8d950343
do1phin <do1phin.name@gmail.com>
2022-06-14 19:00:08 +0300

    #32 done!

---
+++ src/components/SetCity/SetCity.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { SetCity } from './SetCity';
+
+const Template: ComponentStory<typeof SetCity> = (args) => {
+  return (
+    <BrowserRouter>
+      <SetCity {...args} />
+    </BrowserRouter>
+  );
+};
+
+export default {
+  argTypes: {
+    locality: {
+      control: {
+        type: 'text',
+      },
+      description: 'Населённый пункт',
+      table: {
+        defaultValue: { summary: 'text' },
+        type: { summary: 'string' },
+      },
+    },
+    onClick: {
+      description: 'Функция вызываемая при клике на элемент',
+      table: {
+        defaultValue: { summary: '() => {}' },
+        type: { summary: 'Function' },
+      },
+    },
+    placeholder: {
+      control: {
+        type: 'text',
+      },
+      description: 'Плейсхолдер, который виден пока не выбран населённый пункт',
+      table: {
+        defaultValue: { summary: 'text' },
+        type: { summary: 'string' },
+      },
+    },
+  },
+  component: SetCity,
+  parameters: {
+    controls: {
+      expanded: true,
+      sort: 'alpha',
+    },
+  },
+  title: 'SetCity',
+} as ComponentMeta<typeof SetCity>;
+
+const props = {
+  locality: 'Курахово',
+};
+
+export const Simple = Template.bind({});
+Simple.args = {
+  ...Simple.args,
+  ...props,
+};
