b58420b2dda4f81d3d2fa2d83d555714846915e3
do1phin <do1phin.name@gmail.com>
2022-06-16 21:39:06 +0300

    #46 done!

---
+++ src/components/FlagDropdown/FlagDropdown.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { FlagDropdown } from './FlagDropdown';
+
+const Template: ComponentStory<typeof FlagDropdown> = (args) => {
+  return (
+    <BrowserRouter>
+      <FlagDropdown {...args} />
+    </BrowserRouter>
+  );
+};
+
+export default {
+  argTypes: {
+    onClick: {
+      description: 'Функция вызываемая при клике на элемент',
+      table: {
+        defaultValue: { summary: '() => {}' },
+        type: { summary: 'Function' },
+      },
+    },
+  },
+  component: FlagDropdown,
+  parameters: {
+    controls: {
+      expanded: true,
+      sort: 'alpha',
+    },
+  },
+  title: 'Flag/FlagDropdown',
+} as ComponentMeta<typeof FlagDropdown>;
+
+const props = {};
+
+export const WithTitle = Template.bind({});
+WithTitle.args = {
+  ...WithTitle.args,
+  ...props,
+};
