e1ffedc77c171131df7f8da8563d457f0aa98e00
do1phin <do1phin.name@gmail.com>
2022-06-18 20:29:23 +0300

    #64 done!

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,12 @@
 import React, { FC } from 'react';
+import './Home.styles.scss';
 
 export const Home: FC = (): JSX.Element => {
-  return <>its home page</>;
+  return (
+    <section className='home'>
+      <div className='home__container'>
+        <div className='home__wrapper'>its home page</div>
+      </div>
+    </section>
+  );
 };
