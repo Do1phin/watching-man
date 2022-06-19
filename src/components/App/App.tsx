370330e0425f4c26c946704a66464c810c845131
do1phin <do1phin.name@gmail.com>
2022-06-19 15:46:38 +0300

    #30 done!

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import './App.styles.scss';
-import '../../../public/styles/normalize.scss';
-import '../../../public/styles/base.scss';
+import '/public/styles/normalize.scss';
+import '/public/styles/base.scss';
 
 import { MainLayout } from '../../layouts/';
 import { About, Home } from '../../pages/';
 import { IssuesList } from '../IssuesList/IssuesList';
 import { IssueDetails } from '../IssueDetails/IssueDetails';
 import { FullMap } from '../FullMap/FullMap';
+import { Organizations } from '../Organizations/Organizations';
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -29,6 +30,7 @@ export const App: FC = (): JSX.Element => {
                 <Route path='/' element={<MainLayout />}>
                   <Route index element={<Home />} />
                   <Route path='/about' element={<About />} />
+                  <Route path='/organizations' element={<Organizations />} />
                   <Route path='/issues' element={<IssuesList />} />
                   <Route path='/issues/:id' element={<IssueDetails />} />
                   <Route path='/map' element={<FullMap />} />
