2813541e8722f699fde050f0fd96ffd20fea8276
do1phin <do1phin.name@gmail.com>
2022-06-19 13:43:03 +0300

    #78 done!

--- src/components/SetCity/SetCity.tsx
+++ src/components/SetCity/SetCity.tsx
@@ -5,6 +5,8 @@ import { useMyPermission } from './hooks/useMyPermission';
 
 import './SetCity.styles.scss';
 
+import gpsIcon from '/public/images/gps-icon.png';
+
 const SetCity: FC = (): JSX.Element => {
   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -26,7 +28,7 @@ const SetCity: FC = (): JSX.Element => {
       <SetCityModal isOpen={isOpen} setIsOpen={setIsOpen} setLocality={setLocality} />
       <div className='set-city' onClick={getMyAddress}>
         <img
-          src='/images/gps-icon.png'
+          src={gpsIcon}
           alt='Set city icon'
           width='20px'
           height='20px'
