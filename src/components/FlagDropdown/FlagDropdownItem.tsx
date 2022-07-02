bfe1a0c66ec7daa2d99948c6ba7813501712869b
do1phin <do1phin.name@gmail.com>
2022-07-02 20:34:41 +0300

    #109 remove inline styles from FlagDropdown

--- src/components/FlagDropdown/FlagDropdownItem.tsx
+++ src/components/FlagDropdown/FlagDropdownItem.tsx
@@ -3,13 +3,7 @@ import React from 'react';
 const FlagDropdownItem = (item) => {
   return (
     <>
-      <img
-        src={item.flag}
-        width='26px'
-        height='18px'
-        alt='Country flag'
-        className='dropdown__flag'
-      />
+      <img src={item.flag} alt='Country flag' className='dropdown__flag' />
       <p className='dropdown__title'>{item.value}</p>
     </>
   );
